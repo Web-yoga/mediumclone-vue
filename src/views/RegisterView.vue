6c7a51fc09ade7cb0bf6ea946e9ea02bbecc4a0c
Alexander Tsvetkov <hyehaeng@gmail.com>
2023-04-16 19:10:06 +0300

    reg action

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -30,7 +30,10 @@
                 placeholder="Password"
               />
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">
+            <button
+              class="btn btn-lg btn-primary pull-xs-right"
+              :disabled="isSubmitting"
+            >
               Sign Up
             </button>
           </form>
@@ -43,9 +46,15 @@
 <script>
 export default {
   name: 'AppRegister',
+  computed: {
+    isSubmitting() {
+      return this.$store.state.auth.isSubmitting;
+    },
+  },
   methods: {
     onSubmit() {
       console.log('submitted form');
+      this.$store.dispatch('register');
     },
   },
 };
